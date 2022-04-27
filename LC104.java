da874976fd7781b37410778ef5c391da7972c3ce
648303143 <648303143@qq.com>
2022-04-27 11:01:21 +0800

    2022.4.26

---
+++ LC104.java
@@ -0,0 +1,17 @@
+/**
+ * @author zhangqingyang02
+ * @date 2022-04-25-20:13
+ */
+public class LC104 {
+}
+
+class Solution104 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
